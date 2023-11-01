4d4c6ea6326da9681016d36dcec482d0ac853fb1
xero7689 <volleyp7689@gmail.com>
2023-11-01 09:02:52 +0800

    Update: Remove unused import statement

--- hr_system/views.py
+++ hr_system/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from django.contrib import messages

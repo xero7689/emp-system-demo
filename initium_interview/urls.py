b9548049c3bc430a1ec87e401170f0e0db86c841
xero7689 <volleyp7689@gmail.com>
2023-11-01 12:08:36 +0800

    Update: Redirect root path to hr_system

--- initium_interview/urls.py
+++ initium_interview/urls.py
@@ -5,11 +5,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", lambda req: redirect('portal/')),
     path("portal/", include('hr_system.urls')),
     path("admin/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
